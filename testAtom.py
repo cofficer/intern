de9e81474c1d159838927b0d38913d6877c0980d
cofficer <christoffer.gahnstrom@hotmail.com>
2016-03-25 17:45:35 +0100

    This is an update

---
+++ testAtom.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+'''
+Trying out python as well as git / github / Atom / all else.
+Task: Function which plots sinewaves.
+
+'''
+import matplotlib.pyplot as plt
+import numpy as np
+print 1+1
+
+def sinePlotter(amplitude, phase, frequency):
+
+
+
+
+    return sineToPlot
